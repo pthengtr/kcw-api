795cacf5606cde3989a6df505818bc3a8afda8da
pthengtr <pthengtr@gmail.com>
2026-04-10 12:05:27 +0700

    add user for job command

--- src/access/config.py
+++ src/access/config.py
@@ -2,6 +2,6 @@ COMMAND_PERMISSIONS = {
     "ยอดขาย": {"admin", "exec"},
     "ประวัติสินค้า": {"admin", "exec", "staff"},
     "สินค้า": {"admin", "exec", "staff"},
-    "job": {"admin", "exec"},
+    "job": {"admin", "exec", "user"},
     "sync": {"admin", "exec", "user"},
 }
